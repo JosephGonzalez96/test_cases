f93590171f9dc5498f3109ba28d9087fe1482b76
JosephGonzalez96 <37914946+JosephGonzalez96@users.noreply.github.com>
2023-06-25 08:32:52 -0500

    adding headless test

--- test_test1.py
+++ test_test1.py
@@ -1,13 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-browser = webdriver.Chrome()
+# Configure Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--start-maximized")
 
-browser.get('https://www.youtube.com/')
+# browser = webdriver.Chrome()
+browser = webdriver.Chrome(options=chrome_options)
 
-elem = browser.find_element(By.NAME, 'search_query')  # Find the search box
-elem.send_keys('por un segundo')
-elem.send_keys(Keys.ENTER)
+# browser.get('https://www.youtube.com/')
+
+# elem = browser.find_element(By.NAME, 'search_query')  # Find the search box
+# elem.send_keys('por un segundo')
+# elem.send_keys(Keys.ENTER)
+# print("Test completed succesfully")
+
+
+browser.get('https://linux.how2shout.com/how-to-install-and-use-chrome-headless-on-ubuntu/')
+
+elem = browser.find_element(By.XPATH, '//*[@class="entry-title"]')  # Find the search box
+text = elem.get_attribute('innerText')
+print(text)
 print("Test completed succesfully")
